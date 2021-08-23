ac893e880c56ce7697433bc2c24a2f89894012bc
NoraElshahat <nelshohat@gmail.com>
2021-08-23 21:23:48 +0200

    add central error handling file

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 require('./src/db/db');
+const { handleError, ErrorHandler } = require('./src/helpers/error');
 const PORT = 5000;
 const app = express();
 
+app.get('/error', (req, res) => {
+  throw new ErrorHandler(500, 'hiiiii found');
+});
+// central handle error
+app.use((err, req, res, next) => {
+  handleError(err, res);
+});
+
+//server listenning
 app.listen(PORT, () => {
   console.log(`your server is running on port ${PORT}`);
 });
